01e9a84000cbc9af9156068e0ddedb79a234aa88
Lucivaldo Castro <lucivaldocb@gmail.com>
2020-09-21 11:17:27 -0300

    Adicionado Font Awesome e CSS reset

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './assets/stylesheets/main.scss'
+import '@fortawesome/fontawesome-free/js/all'
+
 Vue.config.productionTip = false
 
 new Vue({
